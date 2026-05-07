c9a8164f791ca402799c059513712bf640087dfd
Arantxa Fischer <arantxafischerc21@gmail.com>
2026-05-07 19:25:50 -0400

    feat: security Jwt y Validación básica de contraseñas

---
+++ src/main/java/com/example/demo/security/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.demo.security.config;
+
+import com.example.demo.security.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtFilter;
+//  Configuración central de seguridad de la aplicación//
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                // Desactiva la protección contra ataques CSRF ya que usamos JWT.
+                .csrf(csrf -> csrf.disable())
+                // Establece que el servidor no guardará estados de sesión (Stateless).
+                // Cada petición debe ser independiente y autenticada por el token.
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                // Define las reglas de autorización para los endpoints.
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/auth/**").permitAll() // Login y Registro son públicos
+                        .anyRequest().authenticated()           
+                // Registra el filtro de JWT antes del filtro de usuario/contraseña estándar de Spring.
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+}

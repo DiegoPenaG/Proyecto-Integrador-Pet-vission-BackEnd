7afb1051732bc49eae8510d7f242acd25435e2bf
Cristian-DH <cristian.alexis.d.h@gmail.com>
2026-05-12 18:33:28 -0400

    Extend CitaRepository to use JpaRepository
    Se extendió  CitaRepository para que utilice JpaRepository

--- src/main/java/org/example/petvission/cita/repository/CitaRepository.java
+++ src/main/java/org/example/petvission/cita/repository/CitaRepository.java
@@ -1,4 +1,7 @@
 package org.example.petvission.cita.repository;
 
-public interface CitaRepository {
+import org.example.petvission.cita.model.Cita;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CitaRepository extends JpaRepository<Cita, Long> {
 }
